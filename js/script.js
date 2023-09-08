5f8d5ae9a41e785de8a5b1bf715f49212ca88278
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2023-09-08 11:27:32 +0200

    Update newItem() with functionality
    Update newItem() with the functionality of adding new list items to the unordered list in case of a filled input field, and showing a pop-up message in case of an empty input field.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 function newItem(){
+    //1. Adding a new item to the list of items: 
+    let inputValue = $('input').val();
+    let liItem = $('<li></li>');
+    liItem.text(inputValue);
+
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        $('#list').append(liItem);
+    }
 }
 
 $('#button').on('click', () =>{
